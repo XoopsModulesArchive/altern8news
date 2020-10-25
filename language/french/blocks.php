f6450bfb07587d0ac20fb458f65447642a124ea7
mambax7 <mambax7@gmail.com>
2020-10-25 03:38:17 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ language/french/blocks.php
@@ -0,0 +1,12 @@
+<?php
+
+// $Id: blocks.php,v 1.1 2006/03/16 03:22:32 mikhail Exp $
+define('_MB_NEWS_NOTYET', "There isn't a Biggest Story for Today, yet.");
+define('_MB_NEWS_TMRSI', "Today's most read Story is:");
+define('_MB_NEWS_ORDER', 'Order');
+define('_MB_NEWS_DATE', 'Published Date');
+define('_MB_NEWS_HITS', 'Number of Hits');
+define('_MB_NEWS_DISP', 'Display');
+define('_MB_NEWS_ARTCLS', 'articles');
+define('_MB_NEWS_CHARS', 'Length of the title');
+define('_MB_NEWS_LENGTH', ' characters');
